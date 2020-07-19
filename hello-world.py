b40b6f68bb638b5a9c603f8149a97d9c716c8209
yuqing0304 <yuqingelsa@gmail.com>
2020-07-19 12:32:55 +0800

    add sol to hw1

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,10 @@
 ### Write your code here
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+if __name__  == "__main__":
+    
+    t_string = time.strftime('%H:%M:%S',time.localtime(time.time()))
+    print("Hello :)") 
+    print("time = ", t_string)
